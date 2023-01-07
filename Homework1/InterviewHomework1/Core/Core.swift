92da6a9f426858a7af50e343364d9ba7859c3d6a
Vasily Bodnarchuk <vasilybodnarchuk@gmail.com>
2023-01-06 20:23:38 -0800

    inited Core class

---
+++ Homework1/InterviewHomework1/Core/Core.swift
@@ -0,0 +1,24 @@
+//
+//  Core.swift
+//  InterviewHomework1
+//
+//  Created by Vasily Bodnarchuk on 1/6/23.
+//
+
+import UIKit
+
+class Core {
+    private var assembly: Assembly
+    private var router: Routerable
+    
+    init() {
+        assembly = Assembly()
+        router = Router(assembly: assembly)
+    }
+    
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        router.renderRootController(scene: scene, willConnectTo: session, options: connectionOptions)
+    }
+}
--- Homework1/InterviewHomework1/SceneDelegate.swift
+++ Homework1/InterviewHomework1/SceneDelegate.swift
@@ -9,15 +9,18 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-    private lazy var assembly = Assembly()
-    private lazy var router: Routerable = Router(assembly: assembly)
+//    private lazy var assembly = Assembly()
+//    private lazy var router: Routerable = Router(assembly: assembly)
+    
+    private lazy var core = Core()
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-        router.renderRootController(scene: scene, willConnectTo: session, options: connectionOptions)
+        core.scene(scene, willConnectTo: session, options: connectionOptions)
+//        router.renderRootController(scene: scene, willConnectTo: session, options: connectionOptions)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
