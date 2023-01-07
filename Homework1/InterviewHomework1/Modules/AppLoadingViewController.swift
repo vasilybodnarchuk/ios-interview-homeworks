c7bb1340240d4941f38f8605b968f8944413ee16
Vasily Bodnarchuk <vasilybodnarchuk@gmail.com>
2023-01-06 20:45:35 -0800

    refactored `activityIndicator` in `AppLoadingViewController`

--- Homework1/InterviewHomework1/Modules/AppLoadingViewController.swift
+++ Homework1/InterviewHomework1/Modules/AppLoadingViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class AppLoadingViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        view.backgroundColor = .white
+        view.addSubview(createActivityIndicator())
+        DispatchQueue.global(qos: .userInitiated).async {
+            let miliseconds = UInt32.random(in: 500_000...1_000_000)
+            usleep(miliseconds)
+        }
+    }
+    
+    private func createActivityIndicator() -> UIActivityIndicatorView {
+        let activityIndicatorView = UIActivityIndicatorView()
+        activityIndicatorView.center = view.center
+        activityIndicatorView.startAnimating()
+        return activityIndicatorView
     }
 }
