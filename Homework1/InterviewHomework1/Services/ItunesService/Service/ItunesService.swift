0e4fe5ca83f3d87da32f5e90ccbe1d420d164887
Vasily Bodnarchuk <vasilybodnarchuk@gmail.com>
2023-02-08 20:20:19 -0800

    inited `ItunesService`

---
+++ Homework1/InterviewHomework1/Services/ItunesService/Service/ItunesService.swift
@@ -0,0 +1,8 @@
+//
+//  ItunesService.swift
+//  InterviewHomework1
+//
+//  Created by Vasily Bodnarchuk on 2/8/23.
+//
+
+import Foundation
---
+++ Homework1/InterviewHomework1/Services/ItunesService/Service/ItunesServiceable.swift
@@ -0,0 +1,10 @@
+//
+//  ItunesServiceable.swift
+//  InterviewHomework1
+//
+//  Created by Vasily Bodnarchuk on 2/8/23.
+//
+
+import Foundation
+
+protocol ItunesServiceable: AnyObject { }
